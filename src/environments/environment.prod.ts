9f4d2117125a27e3894b73d9acf9adad0abc32e1
mercchang <mercchang@gmail.com>
2022-03-29 13:50:45 -0400

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDQfDnQHvWWGustqR9F05YyAhbj7u5MHIM",
+    authDomain: "cdlab-5e94f.firebaseapp.com",
+    databaseURL: "https://cdlab-5e94f-default-rtdb.firebaseio.com",
+    projectId: "cdlab-5e94f",
+    storageBucket: "cdlab-5e94f.appspot.com",
+    messagingSenderId: "1040322750595",
+    appId: "1:1040322750595:web:3d55f40bbf383b92bad3b6",
+    measurementId: "G-E54M3H7ZYG"
+  }
 };
